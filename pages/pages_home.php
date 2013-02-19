0576fc74dc18c2a02926867f3d6c613565393e51
Chris Papapavlou (server) <chrispapapaulou@gmail.com>
2013-02-19 18:17:03 +0200

    Added Github page

--- pages/pages_home.php
+++ pages/pages_home.php
@@ -2,10 +2,7 @@
     <h1>&nbsp Welcome </h1>
     <div class="entry">
         <div class="title">
-            <a href="https://github.com/chrispap/">
-                Some work of mine on GitHub
-                <img src="<?php echo $this->PATH ?>public/images/github.ico" >
-            </a>
+			...
         </div>
     </div>
 </div>
