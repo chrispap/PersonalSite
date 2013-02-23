12d9d83edd7d2d0418732950133a57863c1a2b28
Chris Papapavlou (server) <chrispapapaulou@gmail.com>
2013-02-23 12:28:27 +0200

    Images enlarge on click

--- pages/portfolio/portfolio_avr.php
+++ pages/portfolio/portfolio_avr.php
@@ -4,17 +4,17 @@
 
 <div class="entry">
     <div class="title"><i>TKN</i>:</div>
-			
+
     <div class="links">
-		<img src="<?php echo $this->IMAGE_PATH; ?>TKN.png" border="0" > <br/>
+        <img src="<?php echo $this->IMAGE_PATH; ?>TKN.png" border="0" onclick="showImage(this.src);" > <br/>
         <a href="https://github.com/chrispap/TKN" > View on GitHub <img src="<?php echo $this->IMAGE_PATH; ?>github.png" > </a> <br>
     </div>
 
-	<ul>
-		<li> A real time network protocol based on serial data transmission over a UART. </li>
-		<li> A network bootloader for AVR ATmega microcontrollers based on the above protocol. </li>
-		<li> A GUI application serving as user interface to the bootloader. </li>
-	<ul>
+    <ul>
+        <li> A real time network protocol based on serial data transmission over a UART. </li>
+        <li> A network bootloader for AVR ATmega microcontrollers based on the above protocol. </li>
+        <li> A GUI application serving as user interface to the bootloader. </li>
+    <ul>
 
 </div>
 <br>
--- pages/portfolio/portfolio_cpp.php
+++ pages/portfolio/portfolio_cpp.php
@@ -4,27 +4,27 @@
 
 <div class="entry">
     <div class="title"><i> 3D Model Proccessing </i>:</div>
-    
+
     <div class="links">
-		<img src="<?php echo $this->IMAGE_PATH; ?>BVL_3.png" border="0" > <br/>
+        <img src="<?php echo $this->IMAGE_PATH; ?>BVL_3.png" border="0" onclick="showImage(this.src);" > <br/>
         <ul>
-			<li> <a href="https://github.com/chrispap/GraphicsProject" > View on GitHub <img src="<?php echo $this->IMAGE_PATH; ?>github.png" > </a> </li>
-			<li> <a href="<?php echo $this->PUBLIC_PATH; ?>downloads/3D.zip"> Download this application (Windows)<img src="<?php echo $this->IMAGE_PATH; ?>download.png" > </a> </li> 
-		</ul>
+            <li> <a href="https://github.com/chrispap/GraphicsProject" > View on GitHub <img src="<?php echo $this->IMAGE_PATH; ?>github.png" > </a> </li>
+            <li> <a href="<?php echo $this->PUBLIC_PATH; ?>downloads/3D.zip"> Download this application (Windows)<img src="<?php echo $this->IMAGE_PATH; ?>download.png" > </a> </li>
+        </ul>
     </div>
 </div>
 
 <div class="entry">
     <div class="title"><i> Subtitles </i>:</div>
-    
-	Προβάλετε υπότιτλους <i> πάνω </i> από ένα online stremaing video.
-	
+
+    Προβάλετε υπότιτλους <i> πάνω </i> από ένα online stremaing video.
+
     <div class="links">
-		<img src="<?php echo $this->IMAGE_PATH; ?>Subtitles.png" border="0" > <br/>
+        <img src="<?php echo $this->IMAGE_PATH; ?>Subtitles.png" border="0" onclick="showImage(this.src);" > <br/>
         <ul>
-			<li> <a href="https://github.com/chrispap/Subtitles" > View on GitHub <img src="<?php echo $this->IMAGE_PATH; ?>github.png" > </a> </li>
-			<li> <a href="<?php echo $this->PUBLIC_PATH; ?>downloads/Subtitles.zip"> Download this application (Windows)<img src="<?php echo $this->IMAGE_PATH; ?>download.png" > </a> </li> 
-		</ul>
+            <li> <a href="https://github.com/chrispap/Subtitles" > View on GitHub <img src="<?php echo $this->IMAGE_PATH; ?>github.png" > </a> </li>
+            <li> <a href="<?php echo $this->PUBLIC_PATH; ?>downloads/Subtitles.zip"> Download this application (Windows)<img src="<?php echo $this->IMAGE_PATH; ?>download.png" > </a> </li>
+        </ul>
     </div>
 </div>
 
