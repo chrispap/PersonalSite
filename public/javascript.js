e81ee3135d69aebc38b736ef4e4338395ec51ea0
Chris Papapavlou (server) <chrispapapaulou@gmail.com>
2013-04-22 12:59:30 +0300

    Added again public dir, but without downloads

---
+++ public/javascript.js
@@ -0,0 +1,51 @@
+function about()
+{
+    alert("chrispapapaulou@gmail.com");
+}
+
+function clearText(textfield, defval)
+{
+    if (textfield.value == defval ) {
+                $(textfield).val("");
+    }
+
+}
+
+function restoreText(textfield, defval)
+{
+    if (textfield.value == "" ) {
+        $(textfield).val(defval);
+    }
+
+}
+
+function panelActivated ( event, ui )
+{
+    try {
+        active = $("#accordion" ).accordion( "option", "active" );
+        text = $("#accordion h3").eq(active).text();
+        newUrl = PATH + "links/" + text.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        window.history.pushState("","",newUrl);
+    } catch (exc) {
+    }
+}
+
+
+function showImage(imgName) {
+    document.getElementById('largeImg').src = imgName;
+    showLargeImagePanel();
+    unselectAll();
+}
+
+function showLargeImagePanel() {
+    document.getElementById('largeImgPanel').style.visibility = 'visible';
+}
+
+function unselectAll() {
+    if(document.selection) document.selection.empty();
+    if(window.getSelection) window.getSelection().removeAllRanges();
+}
+
+function hideMe(obj) {
+    obj.style.visibility = 'hidden';
+}
